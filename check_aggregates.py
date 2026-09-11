0a45fed77aa619dae80422a7fe184670ee20cfbf
bikram-singh <bikram23march@gmail.com>
2026-09-11 22:47:55 +0530

    Add BigQuery verification scripts (workaround for bq CLI Python 3.14 incompatibility)

---
+++ check_aggregates.py
@@ -0,0 +1,5 @@
+﻿from google.cloud import bigquery
+c = bigquery.Client(project='project-streaming-telemetry')
+query = "SELECT * FROM `telemetry_analytics.telemetry_aggregates` ORDER BY calculated_at DESC LIMIT 10"
+for row in c.query(query).result():
+    print(dict(row))
