3ff28a6d22e258ac4816a7f0eb35bbffb2c7fa13
bikram-singh <bikram23march@gmail.com>
2026-09-11 22:50:01 +0530

    Remove BigQuery verification scripts

--- check_aggregates.py
+++
@@ -1,5 +0,0 @@
-﻿from google.cloud import bigquery
-c = bigquery.Client(project='project-streaming-telemetry')
-query = "SELECT * FROM `telemetry_analytics.telemetry_aggregates` ORDER BY calculated_at DESC LIMIT 10"
-for row in c.query(query).result():
-    print(dict(row))
--- check_incidents.py
+++
@@ -1,5 +0,0 @@
-﻿from google.cloud import bigquery
-c = bigquery.Client(project='project-streaming-telemetry')
-query = "SELECT * FROM `telemetry_analytics.incident_log` ORDER BY detected_at DESC LIMIT 5"
-for row in c.query(query).result():
-    print(dict(row))
